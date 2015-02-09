2bf18bc5d2141cb834d1fa088ad52f024f1f7b9f
Eric Biggers <ebiggers3@gmail.com>
2015-02-09 00:43:15 -0600

    try rep + lit + rep0

--- src/lzms_compress.c
+++ src/lzms_compress.c
@@ -1347,6 +1347,66 @@ begin:
 						(cur_node + l)->num_extra_items = 0;
 					}
 				} while (++l <= len);
+
+
+				/* try rep + lit + rep0  */
+				if (in_end - (in_next + len + 1) >= 2 &&
+				    load_u16_unaligned(in_next + len + 1) ==
+				    load_u16_unaligned(matchptr + len + 1))
+				{
+					u32 next_len = lz_extend(in_next + len + 1,
+								 matchptr + len + 1,
+								 2,
+								 min(c->mf.nice_match_len,
+								     in_end - (in_next + len + 1)));
+
+					unsigned main_state = cur_node->state.main_state;
+					unsigned match_state = cur_node->state.match_state;
+					unsigned lz_match_state = cur_node->state.lz_match_state;
+					unsigned lz_repmatch0_state = cur_node->state.lz_repmatch_states[0];
+
+					main_state = ((main_state << 1) | 1) % LZMS_NUM_MAIN_STATES;
+					match_state = ((match_state << 1) | 0) % LZMS_NUM_MATCH_STATES;
+					lz_match_state = ((lz_match_state << 1) | 1) % LZMS_NUM_LZ_MATCH_STATES;
+
+					lz_repmatch0_state <<= 1;
+					if (rep_idx > 0)
+						lz_repmatch0_state |= 1;
+					lz_repmatch0_state %= LZMS_NUM_LZ_REPMATCH_STATES;
+
+					u32 cost = base_cost +
+						   lzms_fast_length_cost(c, len) +
+						   lzms_bit_cost(0, main_state, c->main_prob_entries) +
+						   ((u32)c->literal_lens[*(in_next + len)] << LZMS_COST_SHIFT);
+
+					main_state = ((main_state << 1) | 0) % LZMS_NUM_MAIN_STATES;
+
+					cost += lzms_bit_cost(1, main_state,
+							      c->main_prob_entries) +
+						lzms_bit_cost(0, match_state,
+							      c->match_prob_entries) +
+						lzms_bit_cost(1, lz_match_state,
+							      c->lz_match_prob_entries) +
+						lzms_bit_cost(0, lz_repmatch0_state,
+							      c->lz_repmatch_prob_entries[0]) +
+						lzms_fast_length_cost(c, next_len);
+
+					u32 total_len = len + 1 + next_len;
+
+					while (end_node < cur_node + total_len)
+						(++end_node)->cost = INFINITE_COST;
+
+					if (cost < (cur_node + total_len)->cost) {
+						(cur_node + total_len)->cost = cost;
+						(cur_node + total_len)->item =
+							((u64)0 << ITEM_SOURCE_SHIFT) | next_len;
+						(cur_node + total_len)->extra_items[0] =
+							((u64)*(in_next + len) << ITEM_SOURCE_SHIFT) | 1;
+						(cur_node + total_len)->extra_items[1] =
+							((u64)rep_idx << ITEM_SOURCE_SHIFT) | len;
+						(cur_node + total_len)->num_extra_items = 2;
+					}
+				}
 			}
 		}
 
