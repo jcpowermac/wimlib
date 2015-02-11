1bf99cc8f17a014b1fed401ede50ae0c34cf8b30
Eric Biggers <ebiggers3@gmail.com>
2015-02-10 20:12:09 -0600

    1

--- include/wimlib/lzms_constants.h
+++ include/wimlib/lzms_constants.h
@@ -11,28 +11,27 @@
 #define LZMS_MIN_MATCH_LEN			1
 #define LZMS_MAX_MATCH_LEN			1073809578
 
-/* The largest allowed match offset, in bytes.  (The smallest is 1.)  */
+/* The smallest and largest allowed match offsets, in bytes  */
+#define LZMS_MIN_MATCH_OFFSET			1
 #define LZMS_MAX_MATCH_OFFSET			1180427428
 
 /* The value to which buffer sizes should be limited.  Microsoft's
- * implementation seems to use only 67108864 (2^26) bytes as the limit, even
- * though the format itself supports much higher match lengths and offsets.
- * We'll use 2^30 as our limit.  */
+ * implementation seems use 67108864 (2^26) bytes.  However, since the format
+ * itself supports higher match lengths and offsets, we'll use 2^30.  */
 #define LZMS_MAX_BUFFER_SIZE			1073741824
 
-/* The number of entries in the LRU queue for offsets of LZ matches  */
+/* Length of the LRU queue for offsets of LZ matches  */
 #define LZMS_NUM_LZ_REPS			3
 
-/* The number of entries in the LRU queue for (power, raw_offset) pairs of delta
- * matches  */
+/* Length of the LRU queue for (power, raw_offset) pairs of delta matches  */
 #define LZMS_NUM_DELTA_REPS			3
 
-/* The maximum number of bits (binary decisions) that are needed to encode an
- * index in the LRU queue for offsets of LZ matches  */
+/* The maximum number of binary decisions that are needed to encode an index in
+ * the LZ match offset LRU queue  */
 #define LZMS_NUM_LZ_REP_DECISIONS		(LZMS_NUM_LZ_REPS - 1)
 
-/* The maximum number of bits (binary decisions) that are needed to encode an
- * index in the LRU queue for (power, raw offset) pairs of LZ matches  */
+/* The maximum number of binary decisions that are needed to encode an index in
+ * the delta match (power, raw offset) pair LRU queue  */
 #define LZMS_NUM_DELTA_REP_DECISIONS		(LZMS_NUM_DELTA_REPS - 1)
 
 /* These values define the precision for probabilities in LZMS, which are always
@@ -44,7 +43,12 @@
 #define LZMS_INITIAL_PROBABILITY		48
 #define LZMS_INITIAL_RECENT_BITS		0x0000000055555555
 
-/* The number of states within each of the various bit contexts  */
+/* The number of probabilities for each of the classes of binary decisions.
+ *
+ * The logarithm base 2 of each of these values is the number of recently
+ * encoded bits that are remembered for each decision class and are used to
+ * select the appropriate probability entry when decoding/encoding the next
+ * decision.  */
 #define LZMS_NUM_MAIN_PROBS			16
 #define LZMS_NUM_MATCH_PROBS			32
 #define LZMS_NUM_LZ_PROBS			64
@@ -59,22 +63,26 @@
 #define LZMS_MAX_NUM_OFFSET_SYMS		799
 #define LZMS_MAX_NUM_SYMS			799
 
-/* Codeword lengths will never exceed this value.  */
-#define LZMS_MAX_CODEWORD_LEN			15
-
-/* The rebuild frequencies (in symbols) for each Huffman code  */
+/* The rebuild frequencies, in symbols, for each Huffman code  */
 #define LZMS_LITERAL_CODE_REBUILD_FREQ		1024
 #define LZMS_LZ_OFFSET_CODE_REBUILD_FREQ	1024
 #define LZMS_LENGTH_CODE_REBUILD_FREQ		512
 #define LZMS_DELTA_OFFSET_CODE_REBUILD_FREQ	1024
 #define LZMS_DELTA_POWER_CODE_REBUILD_FREQ	512
 
-/* The maximum number of "extra bits" needed to encode a match length  */
+/* The maximum length, in bits, of any Huffman codeword.  This is guaranteed by
+ * the way frequencies are updated.  */
+#define LZMS_MAX_CODEWORD_LEN			15
+
+/* The maximum number of verbatim bits (in addition to the Huffman-encoded
+ * length slot symbol) that may be required to encode a match length  */
 #define LZMS_MAX_EXTRA_LENGTH_BITS		30
 
-/* The maximum number of "extra bits" needed to encode a match offset  */
+/* The maximum number of verbatim bits (in addition to the Huffman-encoded
+ * offset slot symbol) that may be required to encode a match offset  */
 #define LZMS_MAX_EXTRA_OFFSET_BITS		30
 
+/* Parameters for x86 machine code pre/post-processing  */
 #define LZMS_X86_ID_WINDOW_SIZE			65535
 #define LZMS_X86_MAX_TRANSLATION_OFFSET		1023
 
